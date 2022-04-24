4569c294aeb4674c9ef95ac71ceafa07e6746caa
TytuZ <xtytuspl@gmail.com>
2022-04-24 12:05:21 +0200

    Poprawiono błąd w sprawdzaniu errorcount oraz zmieniono sposób liczenia size

--- main.cpp
+++ main.cpp
@@ -7,7 +7,8 @@
 int main(/*int argc, char* argv[]*/)
 {
     auto start = std::chrono::steady_clock::now();
-    struct stat results;
+    struct stat results1;
+    struct stat results2;
    /* if (argc > 1) { //wczytanie plików jeśli są argumenty
         std::ifstream file1(argv[1], std::ios::binary);
         std::ifstream file2(argv[2], std::ios::binary);
@@ -18,33 +19,32 @@ int main(/*int argc, char* argv[]*/)
     stat(argv[2], &results);
     int size2 = results.st_size;*/
 
-    stat("file1", &results);//rozmiar plików
-    int size1 = results.st_size; //rozmiar do inta
+    std::ifstream file1("file1", std::ios::in | std::ios::binary);//wczytanie pliku
+    stat("file1", &results1);//rozmiar plików
+    long size1 = results1.st_size; //rozmiar do inta
     char* buffer1=new char(size1); //tablica na plik
-    std::ifstream file1("file1", std::ios::in | std::ios::binary); //wczytanie pliku
     file1.read(buffer1, size1); //odczyt danych z pliku
     for (int i = 0; i < size1; i++) {//wyświetlenie danych z pliku
         std::cout << buffer1[i] << std::endl;
     };
+    file1.close();//zamkniecie plików na zakonczenie
 
-    stat("file2", &results);
-    int size2 = results.st_size;
-    char* buffer2=new char(size2);
     std::ifstream file2("file2", std::ios::in | std::ios::binary);
+    stat("file2", &results2);
+    long size2 = results2.st_size;
+    char* buffer2=new char(size2);
     file2.read(buffer2, size2);
     for (int i = 0; i < size2; i++) {
         std::cout << buffer2[i] << std::endl;
     };
+    file2.close();
 
-
-    int errorcount=0; //obliczanie błędów
+    int errorcount=0;//obliczanie błędów
     for (int i = 0; i <= size1; i++) {
-        for (int j = 0; j <= size2; j++) {
-            if (buffer1[i] != buffer2[j]) {
-                errorcount += 1;
-            }
+        if (buffer1[i] != buffer2[i]) {
+            errorcount += 1;
         }
-    }
+        }
     int BER;
 
     if (errorcount != 0) //obliczanie BER
@@ -63,7 +63,4 @@ int main(/*int argc, char* argv[]*/)
     std::cout << "ilosc roznych bitow = " << errorcount << std::endl;
     std::cout << "BER = " << BER << "%" << std::endl;
     std::cout << "czas obliczen = " << std::chrono::duration_cast<std::chrono::seconds>(end - start).count() <<" s." << std::endl;
-
-    file1.close();//zamkniecie plików na zakonczenie
-    file2.close();
 }
