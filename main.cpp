56a0dfd1060f5d6e5ab006ada222cab060584a09
TytuZ <xtytuspl@gmail.com>
2022-04-28 18:16:02 +0200

    Poprawione błędy

--- main.cpp
+++ main.cpp
@@ -4,56 +4,79 @@
 #include <cstdlib>
 #include <chrono>
 
+int hammingDistance(char n1, char n2);
+
 int main(int argc, char* argv[])
 {
     auto start = std::chrono::steady_clock::now(); //początek liczenia czasu
 
-    std::fstream file1(argv[1], std::ios::binary | std::ios::in);//wczytanie pliku
-    file1.seekg(0, std::ios::end); //rozmiar pliku 2
-    int size1 = file1.tellg();
+    std::fstream file1; 
+    file1.open(argv[1], std::ios::binary | std::ios::in);//wczytanie pliku
+    
+    if (true != file1.good()) //sprawdzenie czy plik działa
+    {
+        std::cout << "Nie udało się otworzyć pliku nr 1.";
+    }
+
+    file1.seekg(0, std::ios::end); //rozmiar pliku
+    int size = (int)file1.tellg();
     file1.seekg(0, std::ios::beg);
-    std::cout << size1 << std::endl;
+    std::cout << size << std::endl;
  
-        size1 = size1 * 8; //zmiana rozmiaru z bajtów na bity
-        char* buffer1 = new char(size1); //tablica na plik
-        file1.read(buffer1, size1); //odczyt danych z pliku
+        char * buffer1 = new char[size]; //tablica na plik
+        file1.read(buffer1, size); //odczyt danych z pliku
     
     file1.close();//zamkniecie pliku na zakonczenie
 
-    std::fstream file2(argv[2], std::ios::binary | std::ios::in);
-    file2.seekg(0, std::ios::end);
-    long size2 = file2.tellg();
-    file2.seekg(0, std::ios::beg);
-    std::cout << size2 << std::endl;
+    std::fstream file2;
+    file2.open(argv[2], std::ios::binary | std::ios::in);
 
-        size2 = size2 * 8;
-        char* buffer2 = new char(size2);
-        file2.read(buffer2, size2);
+    if (true != file2.good()) //sprawdzenie czy plik działa
+    {
+        std::cout << "Nie udało się otworzyć pliku nr 2.";
+    }
+
+        char * buffer2 = new char[size];
+        file2.read(buffer2, size);
     
     file2.close();
 
-    int errorcount=0;//obliczanie błędów
-    for (int i = 0; i <= size1; i++) {
-        if (buffer1[i] != buffer2[i]) {
-            errorcount += 1;
-        }
-        }
+    int errorcount = 0;//obliczanie błędów
+    int totcount = 0;
+    for (int i = 0; i < size; i++)
+    {
+        errorcount += hammingDistance(buffer1[i], buffer2[i]); //za każdą iteracją dodajemy wartość zwróconą z funkcji
+        totcount += 8; //za każdą iteracją porównaliśmy 8 bitów = 1 bajt. char = 1 Bajt
+    }
 
-    int BER=0;
+    float BER=0;
     if (errorcount != 0) //obliczanie BER
     {
-        BER = (errorcount / size1) * 100;
+        BER = ((float)errorcount / size) * 100;
     }
     else
     {
         BER = 0;
     }
 
-    auto end = std::chrono::steady_clock::now();
+    auto end = std::chrono::steady_clock::now(); //koniec liczenia czasu
 
     std::cout << "Wyniki porowniani:" << std::endl;
-    std::cout << "ilosc porownanych bitow = " << size1 << std::endl;
+    std::cout << "ilosc porownanych bitow = " << totcount << std::endl;
     std::cout << "ilosc roznych bitow = " << errorcount << std::endl;
     std::cout << "BER = " << BER << "%" << std::endl;
     std::cout << "czas obliczen = " << std::chrono::duration_cast<std::chrono::seconds>(end - start).count() <<" s." << std::endl;
+}
+
+// Function to calculate hamming distance
+int hammingDistance(char n1, char n2)
+{
+    char x = n1 ^ n2;
+    char setBits = 0;
+    while (x > 0)
+    {
+        setBits += x & 1;
+        x >>= 1;
+    }
+    return setBits;
 }
